b2afc9efc6dfd6d234288d32cf9f34758f68b075
ezz2003 <ezz2003@yandex.ru>
2022-09-14 17:14:18 +1000

    load lesson8

---
+++ public/js/CartComponent.js
@@ -0,0 +1,106 @@
+// const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+Vue.component("cart", {
+    data() {
+        return {
+            cartUrl: "/getBasket.json",
+            cartItems: [],
+            // imgCart: "https://placehold.it/50x100",
+            showCart: false,
+        };
+    },
+    mounted() {
+        this.$parent.getJson(`/api/cart`).then((data) => {
+            for (let item of data.contents) {
+                this.$data.cartItems.push(item);
+            }
+        });
+    },
+    methods: {
+        addProduct(item) {
+            let find = this.cartItems.find((el) => el.id_product === item.id_product);
+            if (find) {
+                this.$parent
+                    .putJson(`/api/cart/${find.id_product}`, { quantity: 1 })
+                    .then((data) => {
+                        if (data.result === 1) {
+                            find.quantity++;
+                        }
+                    });
+            } else {
+                const prod = Object.assign({ quantity: 1 }, item);
+                this.$parent.postJson(`/api/cart`, prod).then((data) => {
+                    if (data.result === 1) {
+                        this.cartItems.push(prod);
+                    }
+                });
+            }
+        },
+        // remove(item) {
+        // this.$parent.getJson(`${API}/addToBasket.json`).then((data) => {
+        // if (data.result === 1) {
+        // if (item.quantity > 1) {
+        // item.quantity--;
+        // } else {
+        // this.cartItems.splice(this.cartItems.indexOf(item), 1);
+        // }
+        // }
+        // });
+        // },
+        remove(product) {
+            console.log(`/api/cart/${product.id_product}/${product.product_name}`);
+            if (product.quantity > 1) {
+                this.$parent
+                    .putJson(`/api/cart/${product.id_product}`, {
+                        quantity: -1,
+                    })
+                    .then((data) => {
+                        if (data.result) {
+                            product.quantity--;
+                        }
+                    });
+            } else {
+                this.$parent
+                    .delJson(
+                        `/api/cart/${product.id_product}/${product.product_name}`,
+                        product
+                    )
+                    .then((data) => {
+                        if (data.result) {
+                            this.cartItems.splice(this.cartItems.indexOf(product), 1);
+                        } else {
+                            console.log("error");
+                        }
+                    });
+            }
+        },
+    },
+    template: `<div>
+     <button class="btn-cart" type="button" @click="showCart = !showCart"><img src="img/basket.svg" alt="basket-icon"><span>5</span></button>
+    <div class="cart-block" v-show="showCart">
+    <cart-item v-for="item of cartItems" :key="item.id_product" :img="item.product_photo" :cart-item="item" @remove="remove">
+    </cart-item>
+    </div>
+    </div>
+    `,
+});
+
+Vue.component("cart-item", {
+    props: ["img", "cartItem"],
+    template: `
+    <div class="cart-item">
+                    <div class="product-bio">
+                        <img :src="img" alt="Some img">
+                        <div class="product-desc">
+                            <div class="product-title">{{ cartItem.product_name }}</div>
+                            <div class="product-quantity">Quantity: {{ cartItem.quantity }}</div>
+                            <div class="product-single-price">$ {{ cartItem.price }} each</div>
+                        </div>
+                    </div>
+                    <div class="right-block">
+                        <div class="product-price">{{cartItem.quantity*cartItem.price}}</div>
+                        <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+                    </div>
+                </div>
+    `,
+});
